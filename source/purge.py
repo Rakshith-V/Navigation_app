0cfcb58a9fbfe734b8eb8b6dfa3694d41b903e3a
Rakshith V <46916635+Rakshith-V@users.noreply.github.com>
2019-09-13 16:21:47 +0530

    Updated purge.py

--- source/purge.py
+++ source/purge.py
@@ -1,9 +1,9 @@
 import sqlite3
 import csv
 
-conn = sqlite3.connect('C:\\SQLite\\Budget_Dev.db')
+conn = sqlite3.connect('C:\\source\\db.sqlite3.db')
 cur = conn.cursor()
 
 #delete all data from the table site that logs URL's
-cur.execute("DELETE FROM SITE")
+cur.execute("DELETE FROM LOCATON")
 conn.commit()
